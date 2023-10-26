c9986b99a1cffa62e252fb5823cae3da3af2962a
João Gabriel De Oliveira Arantes <waliarntes@gmail.com>
2023-10-26 10:51:55 -0300

    corrigindo coisas 26/10/23

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function voltarAoTopo() {
+function voltartopo() {
     document.body.scrollTop = 0; 
     document.documentElement.scrollTop = 0; 
 }
 window.onscroll = function() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("btnVoltarTopo").style.display = "block";
+        document.getElementById("voltartopo").style.display = "block";
     } else {
-        document.getElementById("btnVoltarTopo").style.display = "none";
+        document.getElementById("voltartopo").style.display = "none";
     }
 }
+
